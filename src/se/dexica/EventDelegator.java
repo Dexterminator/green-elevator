4dfb01d0d8fca3a78321cfae1e727ed3df718b85
Annica Alstermark <annica@lujon-macbook.local>
2015-03-09 17:18:09 +0100

    Turned two of the three delegating methods into private classes that implements Runnable.

--- src/se/dexica/EventDelegator.java
+++ src/se/dexica/EventDelegator.java
@@ -2,6 +2,7 @@ package se.dexica;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.concurrent.BlockingQueue;
 
 /**
  * Created by dexter on 04/03/15.
@@ -10,11 +11,14 @@ public class EventDelegator implements Runnable {
     private final List<ElevatorController> elevatorControllers;
     private Connector connector;
     private WorkOptimizer workOptimizer;
+    private List<BlockingQueue<Runnable>> executors;
 
-    public EventDelegator(List<ElevatorController> elevatorControllers, Connector connector, WorkOptimizer workOptimizer) {
+    public EventDelegator(List<ElevatorController> elevatorControllers, Connector connector, WorkOptimizer workOptimizer,
+                          List<BlockingQueue<Runnable>> executors) {
         this.elevatorControllers = elevatorControllers;
         this.connector = connector;
         this.workOptimizer = workOptimizer;
+        this.executors = executors;
     }
 
     private void readEvents() throws IOException, InterruptedException {
@@ -28,20 +32,33 @@ public class EventDelegator implements Runnable {
         String[] eventComponents = event.split(" ");
         String commandType = eventComponents[0];
         if (commandType.equals("p")) {
-            delegatePanelEvent(eventComponents[1], eventComponents[2]);
+            executors.get(Integer.parseInt(eventComponents[1]) - 1).put(new DelegatePanelEvent(eventComponents[1], eventComponents[2]));
         } else if (commandType.equals("b")) {
             delegateFloorButtonEvent(eventComponents[1], eventComponents[2]);
         } else if (commandType.equals("f")) {
-            delegatePositionEvent(eventComponents[1], eventComponents[2]);
+            executors.get(Integer.parseInt(eventComponents[1]) - 1).put(new DelegatePositionEvent(eventComponents[1], eventComponents[2]));
         } else if (commandType.equals("v")) {
             delegateVelocityEvent(eventComponents[1]);
         }
     }
 
-    private void delegatePanelEvent(String elevatorString, String floorString) throws InterruptedException {
-        int elevator = Integer.parseInt(elevatorString) - 1;
-        int floor = Integer.parseInt(floorString);
-        elevatorControllers.get(elevator).registerFloorRequest(new FloorButtonRequest(floor, Direction.NONE));
+    private class DelegatePanelEvent implements Runnable {
+        private int elevator;
+        private int floor;
+
+        public DelegatePanelEvent(String elevatorString, String floorString) {
+            this.elevator = Integer.parseInt(elevatorString) - 1;
+            this. floor = Integer.parseInt(floorString);
+        }
+
+        @Override
+        public void run() {
+            try {
+                elevatorControllers.get(elevator).registerFloorRequest(new FloorButtonRequest(floor, Direction.NONE));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     private void delegateFloorButtonEvent(String floorString, String directionString) throws InterruptedException {
@@ -58,10 +75,23 @@ public class EventDelegator implements Runnable {
         workOptimizer.registerFloorButtonRequest(floor, direction);
     }
 
-    private void delegatePositionEvent(String elevatorString, String positionString) throws InterruptedException {
-        int elevator = Integer.parseInt(elevatorString) - 1;
-        float position = Float.parseFloat(positionString);
-        elevatorControllers.get(elevator).updatePosition(position);
+    private class DelegatePositionEvent implements Runnable{
+        int elevator;
+        float position;
+
+        public DelegatePositionEvent(String elevatorString, String positionString) {
+            this.elevator = Integer.parseInt(elevatorString) - 1;
+            this.position = Float.parseFloat(positionString);
+        }
+
+        @Override
+        public void run() {
+            try {
+                elevatorControllers.get(elevator).updatePosition(position);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     private void delegateVelocityEvent(String velocity) {
--- src/se/dexica/Main.java
+++ src/se/dexica/Main.java
@@ -1,8 +1,12 @@
 package se.dexica;
 
+import jdk.nashorn.internal.ir.Block;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 
 public class Main {
     private static final int numberOfElevators = 2;
@@ -10,15 +14,17 @@ public class Main {
     public static void main(String[] args) throws IOException {
         Connector connector = new Connector();
         connector.connect();
+        List<BlockingQueue<Runnable>> executors = new ArrayList<BlockingQueue<Runnable>>();
         CommandSender commandSender = new CommandSender(connector);
         List<ElevatorController> elevatorControllers = new ArrayList<ElevatorController>();
         for (int i = 0; i < numberOfElevators; i++) {
             ElevatorController elevatorController = new ElevatorController(i+1, commandSender);
             elevatorControllers.add(elevatorController);
+            executors.add(new ArrayBlockingQueue<Runnable>(2000));
             new Thread(elevatorController).start();
         }
         WorkOptimizer workOptimizer = new WorkOptimizer(elevatorControllers);
-        new Thread(new EventDelegator(elevatorControllers, connector, workOptimizer)).start();
+        new Thread(new EventDelegator(elevatorControllers, connector, workOptimizer, executors)).start();
         new Thread(workOptimizer).start();
     }
 }
